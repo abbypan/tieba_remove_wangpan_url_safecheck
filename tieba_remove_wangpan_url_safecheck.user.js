32e3457aa44fdcfed5805af1ecedd85ff597b43f
abbypan <abbypan@gmail.com>
2017-03-19 23:29:06 +0800

    if

--- tieba_remove_wangpan_url_safecheck.user.js
+++ tieba_remove_wangpan_url_safecheck.user.js
@@ -6,7 +6,7 @@
 // @version       0.1
 // @author        Abby Pan (abbypan@gmail.com)
 // @description   移除百度贴吧中百度云链接safecheck跳转  
-// @copyright     2017, Abby Pan (http://abbypan.github.com/) 
+// @copyright     2017, Abby Pan (http://abbypan.github.io/) 
 // @include       http://tieba.baidu.com/*
 // @include       https://tieba.baidu.com/*
 // ==/UserScript==
@@ -16,12 +16,13 @@
 
 var path = '//a';
 
-var wordNumTds = document.evaluate(path, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-if (!wordNumTds) return;
+var links = document.evaluate(path, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+if (!links) return;
 
-for (var i = 0; i < wordNumTds.snapshotLength; i++) {
-    var td = wordNumTds.snapshotItem(i);
-    var s = td.textContent || td.innerText || ""; 
-    if(s.match(/http:\/\/pan\.baidu\.com\/share\/link\?/)) continue;
-    td.setAttribute('href', s);
+for (var i = 0; i < links.snapshotLength; i++) {
+    var anode = links.snapshotItem(i);
+    var s = anode.textContent || anode.innerText || ""; 
+    if(s.match(/http:\/\/pan\.baidu\.com\/share\/link\?/)){
+        anode.setAttribute('href', s);
+    }
 }
